083e9a5c11da1ae949668afe50665b7ea6d8f52d
iLikeSpaghetti0837 <90133763+macelivity@users.noreply.github.com>
2021-11-11 19:01:17 +0100

    Added player platform

--- lowPong.js
+++ lowPong.js
@@ -1,3 +1,5 @@
+//playerObj = 
+
 if (typeof(Worker) !== "undefined") {
     webWorker = new Worker("lowPong_WebWorker.js");
     webWorker.onmessage = function(event){
@@ -5,6 +7,10 @@ if (typeof(Worker) !== "undefined") {
     }
 } else {
     alert("Sorry your browser does not support the Web Worker API. Please use a browser with at least the specified version:\nChrome - 4.0 | Edge - 10.0 | Firefox - 3.5 | Safari - 4.0 | Opera - 11.5");
+}
+
+function movePlayer(){
+
 }
 
 function startMovingUp(){
